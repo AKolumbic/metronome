ea539084b690a564e28e145a2c41764f8ad74045
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-09-28 22:02:27 -0700

    sound!

--- react-metronome/src/Metronome.js
+++ react-metronome/src/Metronome.js
@@ -1,19 +1,50 @@
 import React, { Component } from 'react';
+import click1 from './click1.wav';
+import click2 from './click2.wav';
 import './Metronome.css';
 
 class Metronome extends Component {
-  render() {
-    let bpm = 100;
-    let playing = false;
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      playing: false,
+      count: 0,
+      bpm: 100,
+      beatsPerMeasure: 4
+    }
+
+    this.click1 = new Audio(click1);
+    this.click2 = new Audio(click2);
+  }
+
+  handleBPMchange = event => {
+    const bpm = event.target.value;
+    this.setState({ bpm });
+  }
+
+  startStop = () => {
+    this.click1.play();
+  }
+
+  render() {
+    const { playing, bpm } = this.state;
 
     return (
       <div className="metronome">
         <div className="bpm-slider">
           <div>{bpm} beats per minute</div>
-          <input type="range" min="60" max="240" value={bpm} />
+          <input
+            type="range"
+            min="60"
+            max="240"
+            value={bpm}
+            onChange={this.handleBPMchange} 
+          />
         </div>
-        <button>{playing ? 'Stop' : 'Start'}</button>
+        <button onClick={this.startStop}>
+          {playing ? 'Stop' : 'Start'}
+        </button>
       </div>
     );
   }
