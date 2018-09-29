f39f9d4629f7fac3833e4a5579d2bffcd2dc4208
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-09-28 21:45:47 -0700

    renders metronome
    -non functional

--- react-metronome/src/Metronome.js
+++ react-metronome/src/Metronome.js
@@ -3,9 +3,17 @@ import './Metronome.css';
 
 class Metronome extends Component {
   render() {
+    let bpm = 100;
+    let playing = false;
+
+
     return (
-      <div className="App">
-        hi
+      <div className="metronome">
+        <div className="bpm-slider">
+          <div>{bpm} beats per minute</div>
+          <input type="range" min="60" max="240" value={bpm} />
+        </div>
+        <button>{playing ? 'Stop' : 'Start'}</button>
       </div>
     );
   }
