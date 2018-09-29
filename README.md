6bd98c702d3e2a02a73b74b95725f918c3e0e9b2
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-09-28 21:28:54 -0700

    remove react bootstrapped elements

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# metronome
+# Metronome
+
+This is a react project built by following the tutorial in this link:
+https://daveceddia.com/react-practice-projects/
---
+++ react-metronome/src/Metronome.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Metronome.css';
+
+class Metronome extends Component {
+  render() {
+    return (
+      <div className="App">
+        hi
+      </div>
+    );
+  }
+}
+
+export default Metronome;
